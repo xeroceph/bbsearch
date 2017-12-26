856f204239c18abb65dc25423d4a83c692dc74c4
xeroceph <34613149+xeroceph@users.noreply.github.com>
2017-12-26 13:12:54 -0500

    reworked code as class(es); included some test data

--- bbsearch.py
+++ bbsearch.py
@@ -9,40 +9,48 @@ import sys
 import progressbar
 import requests
 
-# open a list from 'domains.txt' locally
-file = open('domains.txt', 'r')
-items = file.readlines()
-domains = [x[:-1] for x in items] # strip newline chars from list
+class bbsearch():
+    def __init__(self):
+        self.item = 0
+        self.hit = 0
+        self.success = []
+        self.failed = []
+        self.loadList()
+        self.execSearch()
+        self.summary()
 
-item  = 0
-hit = 0
-total = len(domains)
-success = []
-failed = []
-
-def progressBar(value, endvalue, bar_length=20):
-
+    def progressBar(self, value, endvalue, bar_length=20):
         percent = float(value) / endvalue
         arrow = '=' * int(round(percent * bar_length)-1) + '>'
         spaces = ' ' * (bar_length - len(arrow))
-
-        sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+        sys.stdout.write("\rExecuting search: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
         sys.stdout.flush()
 
-for i in domains :
-    item += 1
-    progressBar(item, total)
-    url = 'https://' + i + '/security.txt'
-    try:
-        r = requests.get(url, timeout=10)
-        if r.status_code == 200:
-            hit += 1
-            success.append(i)
-    except:
-        failed.append(i)
+    def loadList(self):
+        # open a list from 'domains.txt' locally
+        file = open('domains.txt', 'r')
+        items = file.readlines()
+        self.domains = [x[:-1] for x in items] # strip newline chars from list
+        self.total = len(self.domains)
+        
+    def execSearch(self):
+        for i in self.domains:
+            self.item += 1
+            self.progressBar(self.item, self.total)
+            url = 'https://' + i + '/security.txt'
+            try:
+                r = requests.get(url, timeout=10)
+                if r.status_code == 200:
+                    self.hit += 1
+                    self.success.append(i)
+            except:
+                self.failed.append(i)
 
-failCount = len(failed)
-percentage = (float(hit) / float(total-failCount)) * 100
-print '  ', hit, '/', (total-failCount), ' --- ', format(round(percentage,2)),'%'
-print "Domains:", success
-print "Request failures:", failed
+    def summary(self):
+        failCount = len(self.failed)
+        percentage = (float(self.hit) / float(self.total - failCount)) * 100
+        print '  ', self.hit, '/', (self.total - failCount), ' --- ', format(round(percentage,2)),'%'
+        print "Domains:", self.success
+        print "Request failures:", self.failed
+
+bbsearch()
