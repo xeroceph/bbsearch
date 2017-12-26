63a6c4dd603ba47b923cd57f228f7f94956cc6c8
xeroceph <34613149+xeroceph@users.noreply.github.com>
2017-12-26 15:41:39 -0500

    added threading for query performance

--- bbsearch.py
+++ bbsearch.py
@@ -8,6 +8,7 @@ GNU General Public License GPL v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.ht
 import sys
 import progressbar
 import requests
+from threading import Thread, Lock
 
 class bbsearch():
     def __init__(self):
@@ -15,8 +16,18 @@ class bbsearch():
         self.hit = 0
         self.success = []
         self.failed = []
+        self.list1 = []
+        self.list2 = []
         self.loadList()
-        self.execSearch()
+
+        # execute via two threads for speed
+        t1 = Thread(target=self.execSearch, args=(self.list1,))
+        t2 = Thread(target=self.execSearch, args=(self.list2,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
         self.summary()
 
     def progressBar(self, value, endvalue, bar_length=20):
@@ -32,19 +43,33 @@ class bbsearch():
         items = file.readlines()
         self.domains = [x[:-1] for x in items] # strip newline chars from list
         self.total = len(self.domains)
-        
-    def execSearch(self):
-        for i in self.domains:
+
+        # split into two lists for threading
+        for count, i in enumerate(self.domains):
+            if count % 2 == 0:
+                self.list1.append(i)
+            else:
+                self.list2.append(i)
+
+    def execSearch(self, domains):
+        lock = Lock()
+        for i in domains:
+            lock.acquire()
             self.item += 1
+            lock.release()
             self.progressBar(self.item, self.total)
             url = 'https://' + i + '/security.txt'
             try:
-                r = requests.get(url, timeout=10)
+                r = requests.get(url, timeout=5)
                 if r.status_code == 200:
+                    lock.acquire()
                     self.hit += 1
                     self.success.append(i)
+                    lock.release()
             except:
+                lock.acquire()
                 self.failed.append(i)
+                lock.release()
 
     def summary(self):
         failCount = len(self.failed)
